3fb65753bdc5728046bd49321f477a78c503e339
TheDev024 <thedeveloper024@gmail.com>
2023-05-16 12:53:15 +0400

    Initial commit

---
+++ src/test/kotlin/org/td024/webquizengine/WebQuizEngineApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.td024.webquizengine
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebQuizEngineApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
