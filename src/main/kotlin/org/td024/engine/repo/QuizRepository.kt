d4f4ea41fb554b7ae15005424af1ea3abafa309b
TheDev024 <thedeveloper024@gmail.com>
2023-07-27 12:40:02 +0400

    quiz service and required functions in quiz repo created

--- src/main/kotlin/org/td024/engine/repo/QuizRepository.kt
+++ src/main/kotlin/org/td024/engine/repo/QuizRepository.kt
@@ -1,6 +1,11 @@
 package org.td024.engine.repo
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 import org.td024.engine.entity.Quiz
 
-interface QuizRepository : JpaRepository<Quiz, Long>
+@Repository
+interface QuizRepository : JpaRepository<Quiz, Long> {
+
+    fun findQuizById(id: Long): Quiz?
+}
---
+++ src/main/kotlin/org/td024/engine/service/QuizService.kt
@@ -0,0 +1,26 @@
+package org.td024.engine.service
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException
+import org.springframework.stereotype.Service
+import org.td024.engine.entity.Quiz
+import org.td024.engine.model.Response
+import org.td024.engine.repo.QuizRepository
+
+@Service
+class QuizService(@Autowired private val repository: QuizRepository) {
+
+    fun findQuizById(id: Long) = repository.findQuizById(id)
+
+    fun save(quiz: Quiz): Quiz = repository.save(quiz)
+
+    fun solve(quizId: Long, answer: Int): Response {
+        val quiz = findQuizById(quizId) ?: throw NotFoundException()
+
+        val (success, feedback) =
+            if (quiz.answer == answer) Pair(true, "Congratulations, you're right!")
+            else Pair(false, "Wrong answer! Please, try again.")
+
+        return Response(success, feedback)
+    }
+}
