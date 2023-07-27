d1dbbf5fddc8e33fb4dfae49d957599d2eb4b90f
TheDev024 <thedeveloper024@gmail.com>
2023-07-27 14:33:58 +0400

    database driver added to dependencies for postgresql and application.properties file configured for db connection

--- src/main/kotlin/org/td024/engine/service/QuizService.kt
+++ src/main/kotlin/org/td024/engine/service/QuizService.kt
@@ -1,8 +1,9 @@
 package org.td024.engine.service
 
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.data.crossstore.ChangeSetPersister.NotFoundException
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Service
+import org.springframework.web.server.ResponseStatusException
 import org.td024.engine.entity.Quiz
 import org.td024.engine.model.Response
 import org.td024.engine.repo.QuizRepository
@@ -10,14 +11,14 @@ import org.td024.engine.repo.QuizRepository
 @Service
 class QuizService(@Autowired private val repository: QuizRepository) {
 
-    fun getAll(): List<Quiz> = repository.findAll()
+    fun getAll(): List<Quiz> = repository.findAll().ifEmpty { throw ResponseStatusException(HttpStatus.NO_CONTENT) }
 
-    fun findQuizById(id: Long) = repository.findQuizById(id)
+    fun findQuizById(id: Long) = repository.findQuizById(id) ?: throw ResponseStatusException(HttpStatus.NOT_FOUND)
 
     fun save(quiz: Quiz): Quiz = repository.save(quiz)
 
     fun solve(quizId: Long, answer: Int): Response {
-        val quiz = findQuizById(quizId) ?: throw NotFoundException()
+        val quiz = findQuizById(quizId)
 
         val (success, feedback) =
             if (quiz.answer == answer) Pair(true, "Congratulations, you're right!")
